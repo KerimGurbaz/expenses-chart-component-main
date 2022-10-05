eb73177c53644e81c3cced29adb52870c72a7d28
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-05 23:26:33 +0200

    updated

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const myDay = document.querySelectorAll(".days_li li");
+console.log(myDay)
 const data = async() => {
     const res = await fetch("data.json")
     console.log(res);
     const data = await res.json()
     console.log(data);
-   
 
       const {day, amount} = data;
+
+
       data.map(({day, amount},i)=>{
         console.log(`${day} ${amount} ${i}`)
        console.log( `myDay[${i}]`);
 
-//    console.log(myDay[i].firstElementChild);
+      console.log(myDay[i]);
+
 console.log(myDay[i].firstElementChild.style.height =`${(amount*2).toFixed(0)}px`)
 myDay[i].firstElementChild.style.backgroundColor ="hsl(10, 79%, 65%)";
 
-       
 
-      
       })
+
       
 }
 
 console.log(data());
+
